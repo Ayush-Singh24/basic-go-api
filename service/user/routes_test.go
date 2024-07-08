6520e9172b63a15439113479f964dd3f2c61ff4c
Ayush Singh <asayushsingh80@gmail.com>
2024-07-08 17:24:28 +0530

    feat: register users with tests

---
+++ service/user/routes_test.go
@@ -0,0 +1,82 @@
+package user
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/Ayush-Singh24/basic-go-api/types"
+	"github.com/gorilla/mux"
+)
+
+func TestUserServiceHandlers(t *testing.T) {
+	userStore := &mockUserStore{}
+	handler := NewHandler(userStore)
+
+	t.Run("should fail if the user payload is invalid", func(t *testing.T) {
+		payload := types.RegisterUserPayload{
+			FirstName: "user",
+			LastName:  "123",
+			Password:  "pass",
+			Email:     "invalid",
+		}
+
+		marshalled, _ := json.Marshal(payload)
+		req, err := http.NewRequest(http.MethodPost, "/register", bytes.NewBuffer(marshalled))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		router := mux.NewRouter()
+
+		router.HandleFunc("/register", handler.handleRegister)
+		router.ServeHTTP(rr, req)
+
+		if rr.Code != http.StatusBadRequest {
+			t.Errorf("expected status code %d , got %d", http.StatusBadRequest, rr.Code)
+		}
+	})
+
+	t.Run("should correctly register the user", func(t *testing.T) {
+		payload := types.RegisterUserPayload{
+			FirstName: "user",
+			LastName:  "123",
+			Password:  "pass",
+			Email:     "valid@gmail.com",
+		}
+
+		marshalled, _ := json.Marshal(payload)
+		req, err := http.NewRequest(http.MethodPost, "/register", bytes.NewBuffer(marshalled))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		router := mux.NewRouter()
+
+		router.HandleFunc("/register", handler.handleRegister)
+		router.ServeHTTP(rr, req)
+
+		if rr.Code != http.StatusCreated {
+			t.Errorf("expected status code %d , got %d", http.StatusBadRequest, rr.Code)
+		}
+	})
+}
+
+type mockUserStore struct{}
+
+func (m *mockUserStore) GetUserByEmail(email string) (*types.User, error) {
+	return nil, fmt.Errorf("user not found")
+}
+
+func (m *mockUserStore) GetUserById(id int) (*types.User, error) {
+	return nil, nil
+}
+
+func (m *mockUserStore) CreateUser(user types.User) error {
+	return nil
+}
